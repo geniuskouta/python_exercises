7024185ecf70b762cf89b2f204e9289cfb46a5e1
geniuskouta <knakano95@gmail.com>
2020-08-12 18:27:28 +0900

    removed print

--- newton_raphson/test.py
+++ newton_raphson/test.py
@@ -5,6 +5,5 @@ epsilon = 0.01
 k = 24.0
 guess = k/2.0
 while abs(guess**2 - k) >= epsilon:
-    print guess
     guess = guess - ((guess**2 - k) / (2*guess))
 print('Square root of', k, 'is about', guess)
