90284bb29d417c996a68bf7588391774499c86a3
geniuskouta <knakano95@gmail.com>
2020-08-12 18:24:49 +0900

    used newton method to get a square root of x**2 - 24

---
+++ newton_raphson/test.py
@@ -0,0 +1,10 @@
+#Newton-Raphson for square root
+#Find x such that x**2 - 24 is within epsilon of 0
+
+epsilon = 0.01
+k = 24.0
+guess = k/2.0
+while abs(guess**2 - k) >= epsilon:
+    print guess
+    guess = guess - ((guess**2 - k) / (2*guess))
+print('Square root of', k, 'is about', guess)
